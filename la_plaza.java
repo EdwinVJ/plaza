69bd4b05d3d49d4c1b5830300ad3d97e846824f5
EdwinVJ <50158270+EdwinVJ@users.noreply.github.com>
2020-07-08 20:34:35 -0500

    Update la_plaza.java

--- la_plaza.java
+++ la_plaza.java
@@ -5,38 +5,39 @@ public class la_plaza {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Ancho de la Plaza: ");
-		int ancho = sc.nextInt();
+		//System.out.print("Ancho de la Plaza: ");
+		long ancho = sc.nextInt();
 		
-		System.out.print("Largo de la plaza: ");
-		int largo = sc.nextInt();
+		//System.out.print("Largo de la plaza: ");
+		long largo = sc.nextInt();
 
-		System.out.print("Ancho loseta: ");
-		int aloseta = sc.nextInt();
+		//System.out.print("Ancho loseta: ");
+		long aloseta = sc.nextInt();
 		
-		if (ancho >=1 && largo >= 1 && aloseta<109) {
+		//if (ancho >=1 && largo >= 1 && aloseta<109) {
 			pesoSandia(ancho, largo, aloseta);
-		}else {
-			System.out.println("Ingresar números positivos....");
-		}
+		//}else {
+			//System.out.println("Ingresar nÃºmeros positivos....");
+		//}
 		
 		sc.close();
 
 	}
 	
-	public static void pesoSandia(int ancho, int largo,int aloseta) {
+	public static void pesoSandia(long ancho, long largo,long aloseta) {
 
 		if(ancho%aloseta == 0 && largo%aloseta == 0) {
-			System.out.println("Losetas requeridas: "+((ancho/aloseta)*(largo/aloseta)));
+			long cc=(ancho/aloseta)*(largo/aloseta);
+			System.out.println(+cc);
 		
 		}else if(ancho%aloseta == 0 && largo%aloseta != 0) {
-			System.out.println("Losetas requeridas: "+((ancho/aloseta)*((largo/aloseta)+1)));
+			System.out.println(((ancho/aloseta)*((largo/aloseta)+1)));
 		
 		}else if (ancho%aloseta != 0 && largo%aloseta ==0 ) {
-			System.out.println("Losetas requeridas: "+(((ancho/aloseta)+1)*(largo/aloseta)));
+			System.out.println((((ancho/aloseta)+1)*(largo/aloseta)));
 		
 		}else {
-			System.out.println("Losetas requeridas: "+ (((ancho/aloseta)+1)*((largo/aloseta)+1)));
+			System.out.println((((ancho/aloseta)+1)*((largo/aloseta)+1)));
 		}
 		
 		
